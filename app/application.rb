09acfb4cceb5d6c87acb0e6904687ea773bfa6c2
Veronica Bustabad <vbustabad@gmail.com>
2018-07-03 23:59:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,15 @@ class Application
     if req.path.match(/testing/)
       resp.write "Route not found"
       resp.status = 404
-    elsif req.path.match(/items/Figs/)
-    elsif req.path.match(/items/Apples/)
+    elsif req.path.match(/items/)
+      item_name = req.params["item"]
+
+      if @@items.include?(item_name)
+        item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Path Not Found"
     end
