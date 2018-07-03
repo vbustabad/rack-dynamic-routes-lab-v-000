8dfff4ab8948a935e33434e75e13f82c84cea23c
Veronica Bustabad <vbustabad@gmail.com>
2018-07-03 23:43:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,20 @@
 class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/testing/)
+      resp.write "Route not found"
+      resp.status = 404
+    elsif req.path.match(/items/Figs/)
+
+    elsif req.path.match(/items/Apples/)
+    else
+      resp.write "Path Not Found"
+    end
+    resp.finish
+  end 
   
 end
   
