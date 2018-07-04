42c60e4d988a0b8323026d4655dc1797cd486ef7
Veronica Bustabad <vbustabad@gmail.com>
2018-07-04 00:35:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,10 +17,10 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       end
-    else 
+    else
       resp.write "Route not found"
       resp.status = 404
-    end 
+    end
     resp.finish
   end
 
